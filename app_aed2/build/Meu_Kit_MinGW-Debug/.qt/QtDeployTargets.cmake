09bcab0f6833c578a3ceb1094a5626c9f1e48934
williamgoliveira <william.william@discente.ufg.br>
2026-05-28 23:25:28 -0300

    Adiciona esqueleto do front-end Qt na pasta app_aed2

---
+++ app_aed2/build/Meu_Kit_MinGW-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_app_aed2_FILE C:/Users/Willi/OneDrive/Documentos/app_aed2/build/Meu_Kit_MinGW-Debug/app_aed2.exe)
+set(__QT_DEPLOY_TARGET_app_aed2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_app_aed2_RUNTIME_DLLS B:/6.11.1/mingw_64/bin/Qt6Widgets.dll;B:/6.11.1/mingw_64/bin/Qt6Gui.dll;B:/6.11.1/mingw_64/bin/Qt6Core.dll)
